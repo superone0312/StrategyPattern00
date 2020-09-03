8d0cdf46d292bf4dd96bbe8d53e5312ef7e09d47
wood9527 <chao10000@hotmail.com>
2020-09-03 02:52:38 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    var imageStorage = new ImageStorage();
+    imageStorage.store("a", new JpegCompressor(), new BlackAndWhiteFilter());
+    imageStorage.store("b", new PngCompressor(), new BlackAndWhiteFilter());
+  }
+}
